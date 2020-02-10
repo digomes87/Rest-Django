f1d6cc5260f24fbf6c19fcf2afd7cb1bd33e192d
Diego G <diego.gomes87@gmail.com>
2020-02-10 17:53:05 -0300

    add put,get,serializer

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
-# Create your views here.
+
+
+class HelloApiView(APIView):
+    """Teste Api View"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, resquest, format=None):
+        """ Resturn a list of Apiviews"""
+        an_apiview = [
+            'Uses Http mehtods as function(get, post,patch, put , delete)'
+
+        ]
+
+        return Response({'message':'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """create a msg with name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    def put(self, request, pk=None):
+        """Handle updating object"""
+        
