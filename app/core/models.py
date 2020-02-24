ad1971c08630145aca1e323d6db0fa79c7ca11a1
Diego G <diego.gomes87@gmail.com>
2020-02-24 16:14:52 -0300

    admin customezer

--- app/core/models.py
+++ app/core/models.py
@@ -25,6 +25,7 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model"""
     email = models.EmailField(max_length=255, unique=True)
